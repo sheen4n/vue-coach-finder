ac42219e8e793553b819b1b40a09a9360a600a26
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-01-09 13:08:31 +0800

    first commit

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,212 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{ invalid: !firstName.isValid }">
+      <label for="firstname">Firstname</label>
+      <input
+        type="text"
+        name="firstname"
+        id="firstname"
+        v-model.trim="firstName.val"
+        @blur="clearValidity('firstName')"
+      />
+      <p v-if="!firstName.isValid">Firstname must not be empty.</p>
+    </div>
+
+    <div class="form-control" :class="{ invalid: !lastName.isValid }">
+      <label for="lastname">Lastname</label>
+      <input
+        type="text"
+        name="lastname"
+        id="lastname"
+        v-model.trim="lastName.val"
+        @blur="clearValidity('lastName')"
+      />
+      <p v-if="!lastName.isValid">Lastname must not be empty.</p>
+    </div>
+
+    <div class="form-control" :class="{ invalid: !description.isValid }">
+      <label for="description">Description</label>
+      <textarea
+        rows="5"
+        name="description"
+        id="description"
+        v-model.trim="description.val"
+        @blur="clearValidity('description')"
+      />
+      <p v-if="!description.isValid">Description must not be empty.</p>
+    </div>
+
+    <div class="form-control" :class="{ invalid: !rate.isValid }">
+      <label for="rate">Hourly Rate</label>
+      <input
+        type="number"
+        id="rate"
+        v-model.number="rate.val"
+        @blur="clearValidity('rate')"
+      />
+      <p v-if="!rate.isValid">Rate must be a positive number.</p>
+    </div>
+
+    <div class="form-control" :class="{ invalid: !areas.isValid }">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <input
+          type="checkbox"
+          id="frontend"
+          value="frontend"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
+        <label for="frontend">Frontend Development</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="backend"
+          value="backend"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
+        <label for="backend">Backend Development</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="career"
+          value="career"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
+        <label for="career">Career Advisory</label>
+      </div>
+      <p v-if="!areas.isValid">Select at least one specialization.</p>
+    </div>
+
+    <p v-if="!formIsValid">Please fix the above the errors</p>
+    <base-button>Register</base-button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['save-data'],
+  data() {
+    return {
+      firstName: {
+        val: '',
+        isValid: true
+      },
+      lastName: {
+        val: '',
+        isValid: true
+      },
+      description: {
+        val: '',
+        isValid: true
+      },
+      rate: {
+        val: null,
+        isValid: true
+      },
+      areas: {
+        val: [],
+        isValid: true
+      }
+    };
+  },
+  computed: {
+    formIsValid() {
+      return (
+        this.areas.isValid &&
+        this.rate.isValid &&
+        this.firstName.isValid &&
+        this.lastName.isValid &&
+        this.description.isValid
+      );
+    }
+  },
+  methods: {
+    clearValidity(field) {
+      this[field].isValid = true;
+    },
+    validateForm() {
+      this.firstName.isValid = !!this.firstName.val;
+      this.lastName.isValid = !!this.lastName.val;
+      this.description.isValid = !!this.description.val;
+      this.rate.isValid = this.rate.val || this.rate.val > 0;
+      this.areas.isValid = !!this.areas.val.length;
+    },
+    submitForm() {
+      this.validateForm();
+
+      if (!this.formIsValid) return;
+
+      const formData = {
+        first: this.firstName.val,
+        last: this.lastName.val,
+        desc: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val
+      };
+      this.$emit('save-data', formData);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,69 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import store from './store/index.js';
+
+const CoachesList = () => import('./pages/coaches/CoachesList.vue');
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
+const NotFound = () => import('./pages/NotFound.vue');
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches'
+    },
+    {
+      path: '/coaches',
+      component: CoachesList
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      props: true,
+      children: [
+        { path: 'contact', component: ContactCoach } // coaches/c1/contact
+      ]
+    },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/auth',
+      component: UserAuth,
+      meta: {
+        requiresUnauth: true
+      }
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
+    }
+  ]
+});
+
+router.beforeEach(function(to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
+});
+
+export default router;
